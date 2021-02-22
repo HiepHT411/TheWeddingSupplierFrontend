bc7a64390affc356e33695ac8fe4aadc36823b1c
HiepHT411 <hoangtuanhiep.04112000@gmail.com>
2021-02-22 23:49:18 +0700

    installed react-router-dom, bootstrap, axios

---
+++ tws-frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <h2>12312312321</h2>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
